be5b15a1f6419cb289050acbe87d5cd8bc6d666f
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-12-28 17:09:34 +0530

    Updated website
    Changed default theme to light and added function to change to dark if required.

--- assets/js/toggle-theme.js
+++ assets/js/toggle-theme.js
@@ -3,13 +3,11 @@ const mainTheme = document.querySelector("#main-theme");
 const shuffleTheme = document.querySelector("#shuffle-theme");
 const toggleIcon = document.querySelector("#toggle-icon");
 toggle.addEventListener("click", function() {
-    if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) || 
-        (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css")) {
+    if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
         mainTheme.href = "assets/css/light.css";
         shuffleTheme.href = "assets/css/light-shuffle.css";
         toggleIcon.className = "icofont-moon";
-    } else if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) || 
-        (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css")) {
+    } else if (mainTheme.getAttribute("href") == "assets/css/light.css" && shuffleTheme.getAttribute("href") == "assets/css/light-shuffle.css") {
         mainTheme.href = "assets/css/dark.css";
         shuffleTheme.href = "assets/css/dark-shuffle.css";
         toggleIcon.className = "icofont-sun-alt";

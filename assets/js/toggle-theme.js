117c7c33b46c8686b7685bdd6b563eef97eeabd0
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-12-28 16:21:43 +0530

    Updated website
    Added dynamic light/dark mode support

---
+++ assets/js/toggle-theme.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector(".toggle-theme");
+const mainTheme = document.querySelector("#main-theme");
+const shuffleTheme = document.querySelector("#shuffle-theme");
+const toggleIcon = document.querySelector("toggle-icon");
+toggle.addEventListener("click", function() {
+    if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
+        mainTheme.href = "assets/css/light.css";
+        shuffleTheme.href = "assets/css/light-shuffle.css";
+        toggleIcon.className = "icofont-moon";
+    } else {
+        mainTheme.href = "assets/css/dark.css";
+        shuffleTheme.href = "assets/css/dark-shuffle.css";
+        toggleIcon.className = "icofont-sun-alt";
+    }
+});

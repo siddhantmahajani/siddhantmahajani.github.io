169a33f694dd61075d5a0eb1938108ede3047028
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-12-28 16:27:06 +0530

    Update toggle-theme.js
    Fixed icon change issue

--- assets/js/toggle-theme.js
+++ assets/js/toggle-theme.js
@@ -1,7 +1,7 @@
 const toggle = document.querySelector(".toggle-theme");
 const mainTheme = document.querySelector("#main-theme");
 const shuffleTheme = document.querySelector("#shuffle-theme");
-const toggleIcon = document.querySelector("toggle-icon");
+const toggleIcon = document.querySelector("#toggle-icon");
 toggle.addEventListener("click", function() {
     if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
         mainTheme.href = "assets/css/light.css";

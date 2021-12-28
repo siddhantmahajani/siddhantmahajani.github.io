b8d69a8153aa2bc915887c55b8f4ae2869e079e7
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-12-28 16:38:44 +0530

    Update toggle-theme.js
    Added default theme selection

--- assets/js/toggle-theme.js
+++ assets/js/toggle-theme.js
@@ -3,11 +3,13 @@ const mainTheme = document.querySelector("#main-theme");
 const shuffleTheme = document.querySelector("#shuffle-theme");
 const toggleIcon = document.querySelector("#toggle-icon");
 toggle.addEventListener("click", function() {
-    if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
+    if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) || 
+        (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css")) {
         mainTheme.href = "assets/css/light.css";
         shuffleTheme.href = "assets/css/light-shuffle.css";
         toggleIcon.className = "icofont-moon";
-    } else {
+    } else if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) || 
+        (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css")) {
         mainTheme.href = "assets/css/dark.css";
         shuffleTheme.href = "assets/css/dark-shuffle.css";
         toggleIcon.className = "icofont-sun-alt";

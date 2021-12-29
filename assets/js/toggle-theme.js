b9e525b01a1a243d64a1012e19af0a4170786d9d
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-12-29 10:54:40 +0530

    Update
    Website update

--- assets/js/toggle-theme.js
+++ assets/js/toggle-theme.js
@@ -3,13 +3,13 @@ const mainTheme = document.querySelector("#main-theme");
 const shuffleTheme = document.querySelector("#shuffle-theme");
 const toggleIcon = document.querySelector("#toggle-icon");
 toggle.addEventListener("click", function() {
-    if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
-        mainTheme.href = "assets/css/light.css";
-        shuffleTheme.href = "assets/css/light-shuffle.css";
-        toggleIcon.className = "icofont-moon";
-    } else if (mainTheme.getAttribute("href") == "assets/css/light.css" && shuffleTheme.getAttribute("href") == "assets/css/light-shuffle.css") {
+    if (mainTheme.getAttribute("href") == "assets/css/light.css" && shuffleTheme.getAttribute("href") == "assets/css/light-shuffle.css") {
         mainTheme.href = "assets/css/dark.css";
         shuffleTheme.href = "assets/css/dark-shuffle.css";
         toggleIcon.className = "icofont-sun-alt";
+    } else if (mainTheme.getAttribute("href") == "assets/css/dark.css" && shuffleTheme.getAttribute("href") == "assets/css/dark-shuffle.css") {
+        mainTheme.href = "assets/css/light.css";
+        shuffleTheme.href = "assets/css/light-shuffle.css";
+        toggleIcon.className = "icofont-moon";
     }
 });

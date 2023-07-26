95db612d6eef4bf9edbc572e6eef1adaae1632ef
Siddhant Mahajani <sidhantmahajani@gmail.com>
2023-07-26 23:33:07 +0100

    update 26/07/2023 - II
    updated title of the web pages and format of the titles.

--- assets/js/toggle-title.js
+++ assets/js/toggle-title.js
@@ -4,21 +4,21 @@ const contact = document.querySelector(".contact-title");
 const resume = document.querySelector(".resume-title");
 header.addEventListener("click", function() {
     if (header.getAttribute("href") == "#header") {
-        document.title = "Home | Siddhant Mahajani";
+        document.title = "home | siddhant mahajani";
     }
 });
 about.addEventListener("click", function() {
     if (about.getAttribute("href") == "#about") {
-        document.title = "About | Siddhant Mahajani";
+        document.title = "about | siddhant mahajani";
     }
 });
 resume.addEventListener("click", function() {
     if (resume.getAttribute("href") == "#resume") {
-        document.title = "Resume | Siddhant Mahajani";
+        document.title = "resume | siddhant mahajani";
     }
 });
 contact.addEventListener("click", function() {
     if (contact.getAttribute("href") == "#contact") {
-        document.title = "Contact | Siddhant Mahajani";
+        document.title = "contact | siddhant mahajani";
     }
 });

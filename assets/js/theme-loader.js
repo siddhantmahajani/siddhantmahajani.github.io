becb1d4c9690af13a9a9988f1246754438ea4f61
siddhantmahajani <13731954+siddhantmahajani@users.noreply.github.com>
2025-02-09 17:02:26 +0000

    update 09.02.2025
    ref: removed the loader for now as its not loading correctly

--- assets/js/theme-loader.js
+++ assets/js/theme-loader.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const mainDarkShuffleTheme = "assets/css/dark-shuffle.css";
     const mainLightTheme = "assets/css/light.css";
     const mainLightShuffleTheme = "assets/css/light-shuffle.css";
-    const loaderDarkTheme = "assets/css/dark-loader.css";
-    const loaderLightTheme = "assets/css/light-loader.css";
+    // const loaderDarkTheme = "assets/css/dark-loader.css";
+    // const loaderLightTheme = "assets/css/light-loader.css";
     const mainTheme = document.getElementById("main-theme");
     const shuffleTheme = document.getElementById("shuffle-theme");
-    const loaderTheme = document.getElementById("loader-theme");
+    // const loaderTheme = document.getElementById("loader-theme");
     const iconToggle = document.getElementById("toggle-icon");
   
     // Check if the user's system prefers dark mode
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (prefersDarkScheme) {
       mainTheme.href = mainDarkTheme;
       shuffleTheme.href = mainDarkShuffleTheme;
-      loaderTheme.href = loaderDarkTheme;
+      // loaderTheme.href = loaderDarkTheme;
       iconToggle.className = "icofont-sun";
     } else {
       mainTheme.href = mainLightTheme;
       shuffleTheme.href = mainLightShuffleTheme;
-      loaderTheme.href = loaderLightTheme;
+      // loaderTheme.href = loaderLightTheme;
       iconToggle.className = "icofont-moon";
     }
   });
